41788da0f7444196df38818d1c060667c337a4f7
Noora Turpeinen <turpeinen.n@hotmail.com>
2022-03-23 11:13:19 +0200

    changed server to localhost

--- inc/headers.php
+++ inc/headers.php
@@ -1,5 +1,6 @@
 <?php
-header('Access-Control-Allow-Origin: ' . $_SERVER['HTTP_ORIGIN']);
+// header('Access-Control-Allow-Origin: ' . $_SERVER['HTTP_ORIGIN']);
+header('Access-Control-Allow-Origin: http://localhost');
 header('Access-Control-Allow-Credentials: true');
 header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
 header('Access-Control-Allow-Headers: Content-Type, Accept, Access-Control-Allow-Header');
