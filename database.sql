95e2ba202137a0a918d8c7093763b859db200b9d
Noora Turpeinen <turpeinen.n@hotmail.com>
2022-03-23 10:35:21 +0200

    added code for creating product table

--- database.sql
+++ database.sql
@@ -1,6 +1,16 @@
 use opisk_n1tuno00;
 
-create table tuoteryhma {
+create table tuoteryhma (
     trnro int primary key not null AUTO_INCREMENT,
     trnimi char(30) not null
-};
+);
+
+create table tuote (
+    tuotenro int primary key not null AUTO_INCREMENT,
+    tuotenimi char(30) unique not null,
+    hinta DECIMAL(5,2) not null,
+    kuva char(50) not null,
+    kuvaus char(255) not null,
+    trnro int not null,
+    foreign key (trnro) references tuoteryhma(trnro)
+);
