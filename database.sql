025bcf40ad219b044348bb1c831f1150f12654a5
Noora Turpeinen <turpeinen.n@hotmail.com>
2022-03-28 12:26:39 +0300

    added test rows for tuote table

--- database.sql
+++ database.sql
@@ -18,8 +18,11 @@ create table tuote (
     tuotenro int primary key not null AUTO_INCREMENT,
     tuotenimi char(30) unique not null,
     hinta DECIMAL(5,2) not null,
-    kuva char(50) not null,
-    kuvaus char(255) not null,
+    kuva char(50),
+    kuvaus char(255),
     trnro int not null,
     foreign key (trnro) references tuoteryhma(trnro)
 );
+
+insert into tuote (tuotenimi, hinta, trnro) values ('Kalja', 10.10, 1);
+insert into tuote (tuotenimi, hinta, trnro) values ('Jee', 10.10, 1);
