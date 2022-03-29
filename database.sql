308c35a4514a269d570f77854e61769714ed1737
Noora Turpeinen <turpeinen.n@hotmail.com>
2022-03-29 12:18:34 +0300

    testing how to insert image to a database

--- database.sql
+++ database.sql
@@ -9,22 +9,22 @@ create table tuoteryhma (
     trnimi char(30) unique not null
 );
 
-insert into tuoteryhma (trnimi) values ('Pils');
-insert into tuoteryhma (trnimi) values ('Pale Ale');
-insert into tuoteryhma (trnimi) values ('Wheat beer');
 insert into tuoteryhma (trnimi) values ('Lager');
+insert into tuoteryhma (trnimi) values ('Pale Ale');
+insert into tuoteryhma (trnimi) values ('Pils');
+insert into tuoteryhma (trnimi) values ('Wheat beer');
 
 create table tuote (
     tuotenro int primary key not null AUTO_INCREMENT,
     tuotenimi char(50) unique not null,
     hinta DECIMAL(5,2) not null,
-    kuva char(50),
+    kuva char(255),
     kuvaus char(255),
     trnro int not null,
     foreign key (trnro) references tuoteryhma(trnro)
 );
 
-insert into tuote (tuotenimi, hinta, kuva, trnro) values ('Test beer', 15.00, LOAD_FILE('C:/xampp/htdocs/verkkopalveluprojekti-backend/images/wheat-bottle-can.png'), 1);
+insert into tuote (tuotenimi, hinta, kuva, trnro) values ('Test beer', 15.00, 'C:/xampp/htdocs/verkkopalveluprojekti-backend/images/wheat-bottle-can.png', 1);
 insert into tuote (tuotenimi, hinta, trnro) values ('Test beer 1', 10.50, 1);
 insert into tuote (tuotenimi, hinta, trnro) values ('Test beer 2', 29.99, 2);
 insert into tuote (tuotenimi, hinta, trnro) values ('Test beer 3', 18.00, 3);
