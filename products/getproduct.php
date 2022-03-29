cec7c92760f26e24154171e7ceda7a2cca720d96
Noora Turpeinen <turpeinen.n@hotmail.com>
2022-03-29 11:15:38 +0300

    added file for showing individual products

---
+++ products/getproduct.php
@@ -0,0 +1,19 @@
+<?php
+require_once '../inc/functions.php';
+require_once '../inc/headers.php';
+
+$uri = parse_url(filter_input(INPUT_SERVER, 'PATH_INFO'),PHP_URL_PATH);
+$parameters = explode('/',$uri);
+$product_id = $parameters[1];
+
+try {
+    $db = openDb();
+    $sql = "select * from tuote where tuotenro = $product_id";
+    $query = $db->query($sql);
+    $product = $query->fetch(PDO::FETCH_ASSOC);
+
+    header('HTTP/1.1 200 OK');
+    echo json_encode($product, JSON_PRETTY_PRINT);
+} catch(PDOException $pdoex) {
+    returnError($pdoex);
+}
